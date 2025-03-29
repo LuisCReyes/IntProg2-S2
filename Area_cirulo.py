11eb76cdcfe1c442e8d8c8b242d56055bfc2aa0f
Luis Reyes <lcreyes@uamv.edu.ni>
2025-03-29 10:47:25 -0600

    Delete Area_cirulo.py

--- Area_cirulo.py
+++
@@ -1,8 +0,0 @@
-Bienvenido a su calculadora de área de un circulo
-Porfavor ingrese el radio del círculo: 
-10
-El área del círculo es de: 314.16 metros cuadrado.
-
-
-** Process exited - Return Code: 0 **
-Press Enter to exit terminal
